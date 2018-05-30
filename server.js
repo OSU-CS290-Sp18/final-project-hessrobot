6683f8eeacae3a6b2145250faad9f11f03c7d2e5
Phillip MestasIII <mestasp@kec1130-24.engr.oregonstate.edu>
2018-05-30 14:29:26 -0700

    Setup server things.

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
-Serve stuff
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var MongoClient = require("mongodb").MongoClient;
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mogoDBName = process.env.MONGO_DV;
+
+var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' _ mongoPort + '/' + mongoDBName;
+
+var mongoConnection = null;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+	res.status(200).render('homePage');
+});
+
+MongoClient.connect(mongoURL, function(err,connection){
+	if (err)
+	{
+		throw err;
+	}
+	mongoConnection = connection;
+	app.listen(port,function(){
+		console.log("Server Listening on port: ", port);
+	});
+});
+
+
