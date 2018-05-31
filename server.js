03643e9a3520f4ab843d52bc9bcfa7ac5a3b9b78
Phillip MestasIII <mestasp@flip2.engr.oregonstate.edu>
2018-05-31 15:34:53 -0700

    Server loading...

--- server.js
+++ server.js
@@ -23,12 +23,74 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
-	res.status(200).render('homePage');
+	let eventDataCollection = mongoConnection.collection("eventData");
+	eventDataCollection.find({}).toArray(function (err, results) {
+		if (err)
+		{
+			res.status(500).send("Server side error.");
+		}
+		else
+		{
+			let num = results.length;
+			res.status(200).render('homePage', {events: results, numEvents: num});
+		}
+	});
+});
+
+app.get("/:eventID", function (req, res, next){
+	let eventDataCollection = mongoConnection.collection("eventData");
+
+	eventDataCollection.find({ eventID: req.params.eventID}).toArray(function (err, results){
+		if (err)
+		{
+			res.status(500).send("Error fetching event");
+		}
+		else if (results.length > 0)
+		{
+			res.status(200).render('eventPage', results[0]);
+		}
+		else
+		{
+			next();
+		}
+	});
 });
 
 app.get('*', function(req,res){
 	res.status(404).render("404");
 });
+
+app.post("/addEvent", function (req, res, next){
+	if (req.body)
+	{
+		let eventDataCollection = mongoConnection.collection("eventData");
+		var length = 0;
+		eventDataCollection.find({}).toArray(function (err, results){
+			if (err)
+			{
+				res.status(500).send("Error in database");
+			}
+			length = results.length;
+			let eventObject = {
+				eventName: req.body.eventName,
+				eventId: length
+			};
+			eventDataCollection.insertOne(evebtObject, function(err, results) {
+				if (err){
+					res.status(500).send("Error inserting to DB");
+				}
+				else
+				{
+					res.status(200).send("Success");
+				}
+			});
+		});
+	}
+	else
+	{
+		next();
+	}
+}
 
 app.post('*', function(req,res){
 	res.status(404).send("Attempting to POST to unknown path.");
@@ -44,5 +106,3 @@ MongoClient.connect(mongoURL, function(err,connection){
 		console.log("Server Listening on port: ", port);
 	});
 });
-
-
