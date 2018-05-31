58c63db18bd477e61a8e2706c8d46e8bad2a0e8e
Thang Kim <kimth@flip3.engr.oregonstate.edu>
2018-05-31 15:47:29 -0700

    events posting with error handling for incompleted boxes

--- public/index.js
+++ public/index.js
@@ -25,9 +25,29 @@ function closeModal(){
 	modal.classList.add("hidden");	
 	console.log("close dat hun");
 }
+
+function checkValidPost(){
+	var title = document.getElementById("event-title-input");
+	var description = document.getElementById("event-description-input");
+	var locations = document.getElementById("event-location-input");
+	var time = document.getElementById("event-time-input");
+	var capacity = document.getElementById("event-capacity-input");
+	if(title.value=='' || description.value=='' || locations.value=='' || time.value=='' || capacity.value==''){
+		alert("you must fill out all boxes");
+		closeModal();
+	}
+	else{
+		console.log("valid post");
+		closeModal();
+	}
+}
+
+
+var makeEvent = document.getElementsByClassName("modal-accept-button")[0];
 	
 var postModalButton = document.getElementById("create-event-button");
 
 var closeModalButton = document.getElementsByClassName("modal-close-button")[0];
 closeModalButton.addEventListener("click",closeModal);
 postModalButton.addEventListener("click",openModal);
+makeEvent.addEventListener("click",checkValidPost);
