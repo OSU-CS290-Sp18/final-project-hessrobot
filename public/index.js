ad2b8ad42da647f2f5dd8872163efa884142cc05
Thang Kim <kimth@flip1.engr.oregonstate.edu>
2018-06-06 19:52:02 -0700

    LK copy of index.js

--- public/index.js
+++ public/index.js
@@ -42,12 +42,25 @@ function checkValidPost(){
 	}
 }
 
+function sidebutton(){
+	alert("fuk u");
+}
 
+//Filter buttons
+var sport = document.getElementById("sports-button");
+var food = document.getElementById("food-button");
+var study = document.getElementById("study-button");
+var chill = document.getElementById("chill-button");
+var party = document.getElementById("party-button");
+var other = document.getElementById("other-button");
+sport.addEventListener("click",sidebutton);
 var makeEvent = document.getElementsByClassName("modal-accept-button")[0];
 	
 var postModalButton = document.getElementById("create-event-button");
 
 var closeModalButton = document.getElementsByClassName("modal-close-button")[0];
+
+
 closeModalButton.addEventListener("click",closeModal);
 postModalButton.addEventListener("click",openModal);
 makeEvent.addEventListener("click",checkValidPost);
